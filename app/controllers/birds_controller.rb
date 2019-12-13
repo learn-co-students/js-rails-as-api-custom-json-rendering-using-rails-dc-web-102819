314a4e4b1ee20c441ef6ccdf0a1179ad1744dfcf
jasur <tarancci@gmail.com>
2019-12-13 17:55:58 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,17 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: birds, only: [:id, :name, :species] #or you could use except: [:created_at, updated_at] to exclude those keys
+    else
+      render json: {messages: 'Bird not found'} #renders a message if there is an error due to invalid endpoint
+    end
+    
+  end
+
 end
